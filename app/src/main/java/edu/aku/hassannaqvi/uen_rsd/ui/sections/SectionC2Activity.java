3832e9f4463bcdf3c9c0e36ef3ea449ee26ec8a3
SAJID LATIF <indus1530@gmail.com>
2021-10-27 04:32:30 +1400

    SectionC2Activity Created

---
+++ app/src/main/java/edu/aku/hassannaqvi/uen_rsd/ui/sections/SectionC2Activity.java
@@ -0,0 +1,16 @@
+package edu.aku.hassannaqvi.uen_rsd.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import edu.aku.hassannaqvi.uen_rsd.R;
+
+public class SectionC2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_c2);
+    }
+}
