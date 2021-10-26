bcc4473e66b84c5e02d7a760c8a8fd420d75c387
SAJID LATIF <indus1530@gmail.com>
2021-10-27 04:37:33 +1400

    SectionC2Activity Created

--- app/src/main/java/edu/aku/hassannaqvi/uen_rsd/ui/sections/SectionC1Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_rsd/ui/sections/SectionC1Activity.java
@@ -16,14 +16,12 @@ import com.validatorcrawler.aliazaz.Validator;
 import edu.aku.hassannaqvi.uen_rsd.R;
 import edu.aku.hassannaqvi.uen_rsd.data.model.Form;
 import edu.aku.hassannaqvi.uen_rsd.database.DatabaseHelper;
-import edu.aku.hassannaqvi.uen_rsd.databinding.ActivitySectionCBinding;
+import edu.aku.hassannaqvi.uen_rsd.databinding.ActivitySectionC1Binding;
 import edu.aku.hassannaqvi.uen_rsd.ui.RegisterActivity;
-import edu.aku.hassannaqvi.uen_rsd.ui.TakePhoto;
 
 
 public class SectionC1Activity extends AppCompatActivity {
-    ActivitySectionCBinding bi;
-    int photoCount = 0;
+    ActivitySectionC1Binding bi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +30,6 @@ public class SectionC1Activity extends AppCompatActivity {
         bi.setCallback(this);
         bi.setForm(form);
         setSupportActionBar(bi.toolbar);
-        setupSkips();
-    }
-
-
-    private void setupSkips() {
-        bi.imgcheck.setOnCheckedChangeListener((compoundButton, b) -> bi.f3image.setEnabled(!b));
     }
 
 
@@ -64,7 +56,7 @@ public class SectionC1Activity extends AppCompatActivity {
         if (updateDB()) {
             setResult(2);
             finish();
-            startActivity(new Intent(this, RegisterActivity.class));
+            startActivity(new Intent(this, SectionC2Activity.class));
         }
     }
 
@@ -97,45 +89,6 @@ public class SectionC1Activity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "Failed to update DB", Toast.LENGTH_SHORT).show();
             return false;
-        }
-    }
-
-
-    public void takePhoto(View view) {
-        Intent intent = new Intent(this, TakePhoto.class);
-        /*intent.putExtra("picID", info.getCluster() + "_" + info.getHhno() + "_" + info.getCb01() + "_");
-        intent.putExtra("childName", info.getCb02());*/
-        intent.putExtra("picID", bi.f3image.getId() + "_" + bi.txtf3image.getId() + "_" + R.string.f1title + "_");
-        intent.putExtra("imgName", R.string.f3image);
-        intent.putExtra("picView", view.getId());
-        startActivityForResult(intent, 1); // Activity is started with requestCode 1 = Front
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode != RESULT_CANCELED) {
-            Toast.makeText(this, requestCode + "_" + resultCode, Toast.LENGTH_SHORT).show();
-            String fileName = data.getStringExtra("FileName");
-            if (requestCode == 1 && resultCode == 1) {
-                photoCount++;
-                if (photoCount > 0) bi.imgcheck.setVisibility(View.GONE);
-                bi.txtf3image.setText(photoCount + " - " + fileName);
-                bi.txtf3image.setCompoundDrawablesWithIntrinsicBounds(R.drawable.camera_checked, 0, 0, 0);
-                if (photoCount == 5) bi.f3image.setEnabled(false);
-                Toast.makeText(this, "Photo Taken", Toast.LENGTH_SHORT).show();
-            } else if (requestCode == 1 && resultCode != 1) {
-                photoCount = 0;
-                bi.f3image.setEnabled(true);
-                bi.txtf3image.setText(bi.txtf3image.getText().toString());
-                Toast.makeText(this, "Photo Cancelled", Toast.LENGTH_SHORT).show();
-            }
-        } else {
-            if (photoCount == 0) bi.imgcheck.setVisibility(View.VISIBLE);
-            bi.txtf3image.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.camera_unchecked, 0, 0);
-            bi.f3image.setEnabled(true);
-            bi.txtf3image.setText(bi.txtf3image.getText().toString());
-            Toast.makeText(this, "Photo Cancelled", Toast.LENGTH_SHORT).show();
         }
     }
 }
--- app/src/main/java/edu/aku/hassannaqvi/uen_rsd/ui/sections/SectionC2Activity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_rsd/ui/sections/SectionC2Activity.java
@@ -1,16 +1,141 @@
 package edu.aku.hassannaqvi.uen_rsd.ui.sections;
 
+import static edu.aku.hassannaqvi.uen_rsd.core.MainApp.appInfo;
+import static edu.aku.hassannaqvi.uen_rsd.core.MainApp.form;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.validatorcrawler.aliazaz.Validator;
 
 import edu.aku.hassannaqvi.uen_rsd.R;
+import edu.aku.hassannaqvi.uen_rsd.data.model.Form;
+import edu.aku.hassannaqvi.uen_rsd.database.DatabaseHelper;
+import edu.aku.hassannaqvi.uen_rsd.databinding.ActivitySectionC2Binding;
+import edu.aku.hassannaqvi.uen_rsd.ui.RegisterActivity;
+import edu.aku.hassannaqvi.uen_rsd.ui.TakePhoto;
+
 
 public class SectionC2Activity extends AppCompatActivity {
+    ActivitySectionC2Binding bi;
+    int photoCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_section_c2);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_c2);
+        bi.setCallback(this);
+        bi.setForm(form);
+        setSupportActionBar(bi.toolbar);
+        setupSkips();
+    }
+
+
+    private void setupSkips() {
+        bi.imgcheck.setOnCheckedChangeListener((compoundButton, b) -> bi.f3image.setEnabled(!b));
+    }
+
+
+    private void saveDraft() {
+    }
+
+
+    private boolean updateDB() {
+        DatabaseHelper db = appInfo.getDbHelper();
+        int updcount = db.updatesFormColumn(Form.FormsTable.COLUMN_SC, form.sCtoString());
+        if (updcount == 1) {
+            return true;
+        } else {
+            Toast.makeText(this, "SORRY! Failed to update DB", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+    public void btnContinue(View view) {
+        if (!formValidation()) return;
+        if (!addForm()) return;
+        saveDraft();
+        if (updateDB()) {
+            setResult(2);
+            finish();
+            startActivity(new Intent(this, RegisterActivity.class));
+        }
+    }
+
+
+    public void BtnEnd(View view) {
+        finish();
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+
+    private boolean formValidation() {
+        return Validator.emptyCheckingContainer(this, bi.GrpName);
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this, "Back Press Not Allowed", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean addForm() {
+        if (!form.getId().equals("")) return true;
+        DatabaseHelper db = appInfo.dbHelper;
+        long rowid = db.addForm(form);
+        form.setId(String.valueOf(rowid));
+        if (rowid > 0) {
+            form.setUid(form.getDeviceId() + form.getId());
+            db.updatesFormColumn(Form.FormsTable.COLUMN_UID, form.getUid());
+            return true;
+        } else {
+            Toast.makeText(this, "Failed to update DB", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+    }
+
+
+    public void takePhoto(View view) {
+        Intent intent = new Intent(this, TakePhoto.class);
+        /*intent.putExtra("picID", info.getCluster() + "_" + info.getHhno() + "_" + info.getCb01() + "_");
+        intent.putExtra("childName", info.getCb02());*/
+        intent.putExtra("picID", bi.f3image.getId() + "_" + bi.txtf3image.getId() + "_" + R.string.f1title + "_");
+        intent.putExtra("imgName", R.string.f3image);
+        intent.putExtra("picView", view.getId());
+        startActivityForResult(intent, 1); // Activity is started with requestCode 1 = Front
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_CANCELED) {
+            Toast.makeText(this, requestCode + "_" + resultCode, Toast.LENGTH_SHORT).show();
+            String fileName = data.getStringExtra("FileName");
+            if (requestCode == 1 && resultCode == 1) {
+                photoCount++;
+                if (photoCount > 0) bi.imgcheck.setVisibility(View.GONE);
+                bi.txtf3image.setText(photoCount + " - " + fileName);
+                bi.txtf3image.setCompoundDrawablesWithIntrinsicBounds(R.drawable.camera_checked, 0, 0, 0);
+                if (photoCount == 5) bi.f3image.setEnabled(false);
+                Toast.makeText(this, "Photo Taken", Toast.LENGTH_SHORT).show();
+            } else if (requestCode == 1 && resultCode != 1) {
+                photoCount = 0;
+                bi.f3image.setEnabled(true);
+                bi.txtf3image.setText(bi.txtf3image.getText().toString());
+                Toast.makeText(this, "Photo Cancelled", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            if (photoCount == 0) bi.imgcheck.setVisibility(View.VISIBLE);
+            bi.txtf3image.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.camera_unchecked, 0, 0);
+            bi.f3image.setEnabled(true);
+            bi.txtf3image.setText(bi.txtf3image.getText().toString());
+            Toast.makeText(this, "Photo Cancelled", Toast.LENGTH_SHORT).show();
+        }
     }
 }
